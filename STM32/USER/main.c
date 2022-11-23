d1a1b8bac2eb552487440021f10c50df983c0a3c
seengreat <110515049+seengreat@users.noreply.github.com>
2022-11-23 16:05:12 +0800

    Update main.c

--- STM32/USER/main.c
+++ STM32/USER/main.c
@@ -48,34 +48,38 @@ int main(void)
 		Epd_init(); //EPD init			
     Image_Init(EPD_MEM_W, EPD_MEM_H);
 		Gui_Clear();
-    PIC_display_GC(Image.img);	
-		Gui_Clear();
-		Gui_draw_line(1, 1, 250, 1, 2, SOLID);
-		Gui_draw_line(1, 1, 1, 122, 1, SOLID);
-		Gui_draw_rectangle(190, 8, 205, 28, 1, EMPTY);
-		Gui_draw_rectangle(210, 8, 230, 28, 1, FULL);
-		Gui_draw_circle(210, 60, 15, 1, FULL);
-		Gui_draw_circle(210, 60, 20, 1, EMPTY);
-		Gui_draw_str(10, 20, "876543210!", &Font24);
-		Gui_draw_str(10, 50, "seengreat!", &Font24);
-		Gui_draw_str(10, 80, "SEENGREAT!", &Font24);	
-		PIC_display_DU(Image.img);//Partial  refresh display screen, it is recommended to conduct full refresh every 10 DU refresh
-		delay_ms(1000);
-		delay_ms(1000);
-		Gui_Clear();
-		Gui_draw_str(45, 5, "SEENGREAT!", &Font24);			
-		Gui_draw_str(1, 35, "2inch13", &Font24);
-		Gui_draw_str(130, 35, "E-Paper", &Font24);
-		Gui_draw_str(1, 65, "250x122 pixels", &Font24);	
-		Gui_draw_str(1, 95, "Black", &Font24);	
-		Gui_draw_str(92, 95, "and", &Font24);
-		Gui_draw_str(150, 95, "White", &Font24);				
-		PIC_display_GC(Image.img);//GC full refresh display screen, it is recommended to conduct full refresh every 10 DU refresh
-		Epd_sleep();//enter the sleep mode to lock the image display, Otherwise, unexpected stripes may appear on the screen when the power is cut off
-		delay_ms(1000);
-		delay_ms(1000);	
-		delay_ms(1000);
-		delay_ms(1000);				
+    PIC_display_GC(Image.img);//the first time refresh must be GC full mode
+    while(1)
+    {			
+				Gui_Clear();
+				Gui_draw_line(1, 1, 250, 1, 2, SOLID);
+				Gui_draw_line(1, 1, 1, 122, 1, SOLID);
+				Gui_draw_rectangle(190, 8, 205, 28, 1, EMPTY);
+				Gui_draw_rectangle(210, 8, 230, 28, 1, FULL);
+				Gui_draw_circle(210, 60, 15, 1, FULL);
+				Gui_draw_circle(210, 60, 20, 1, EMPTY);
+				Gui_draw_str(10, 20, "876543210!", &Font24);
+				Gui_draw_str(10, 50, "seengreat!", &Font24);
+				Gui_draw_str(10, 80, "SEENGREAT!", &Font24);	
+				PIC_display_DU(Image.img);//Partial  refresh display screen, it is recommended to conduct full refresh every 10 DU refresh
+				delay_ms(1000);
+				delay_ms(1000);
+				Gui_Clear();
+				Gui_draw_str(45, 5, "SEENGREAT!", &Font24);			
+				Gui_draw_str(1, 35, "2inch13", &Font24);
+				Gui_draw_str(130, 35, "E-Paper", &Font24);
+				Gui_draw_str(1, 65, "250x122 pixels", &Font24);	
+				Gui_draw_str(1, 95, "Black", &Font24);	
+				Gui_draw_str(92, 95, "and", &Font24);
+				Gui_draw_str(150, 95, "White", &Font24);				
+				PIC_display_GC(Image.img);//GC full refresh display screen, it is recommended to conduct full refresh every 10 DU refresh
+				Epd_sleep();//enter the sleep mode to lock the image display, Otherwise, unexpected stripes may appear on the screen when the power is cut off
+				delay_ms(1000);
+				delay_ms(1000);	
+				delay_ms(1000);
+				delay_ms(1000);
+				while(1);
+	  }		
 			 
 }
 
