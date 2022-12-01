a7e39c2eeefacfc01a37a4035f79eef7abd99b36
seengreat <110515049+seengreat@users.noreply.github.com>
2022-12-01 19:43:29 +0800

    Update gpio.h

--- STM32/HARDWARE/GPIO/gpio.h
+++ STM32/HARDWARE/GPIO/gpio.h
@@ -5,8 +5,6 @@
 #define	NSS PBout(12)  
 #define	LED PBout(4) 
 
-#define macRC522_Reset_Enable() GPIO_ResetBits( GPIOA, GPIO_Pin_8 )
-#define macRC522_Reset_Disable() GPIO_SetBits( GPIOA, GPIO_Pin_8)
 
 void IO_Init(void);
 
