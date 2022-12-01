2c4808184dbb6b20bb08b3132d7585301bc83c01
seengreat <110515049+seengreat@users.noreply.github.com>
2022-12-01 19:42:51 +0800

    Update spi.c

--- STM32/HARDWARE/SPI/spi.c
+++ STM32/HARDWARE/SPI/spi.c
@@ -1,27 +1,4 @@
-/***************************************************************************************
- * Project  :RC522-SPI-STM32
- * Describe :Read the UID of the S50 card and print the card number
- *			 Read block 8 data and change the first four bytes to 0x11, 0x22, 0x33, 0x44
- * Experimental Platform :STM32F103C8T6 + RC522 RFID HAT
- * Hardware Connection :STM32F103 -> RC522 RFID HAT
- *			5V	 ->	5V					PB12 -> CE0
- *			GND	 ->	GND					PB13 -> SCK
- *			PA8	 ->	P25					PB14 -> MISO
- *			PB4  ->	P29					PB15 -> MOSI
- *			PB5	 ->	P24
- *			
- *	SW1:	RX   -> OFF			SW2:	A1	 -> -
- *          TX   -> OFF					A0	 -> +
- *          SDA  -> OFF					ADR5 -> 0
- *          SCL  -> OFF					ADR4 -> 0
- *          NSS  -> ON					ADR3 -> 0
- *          MOSI -> ON					ADR2 -> 0
- *          MISO -> ON					ADR1 -> 0
- *          SCK  -> ON					ADR0 -> 0
- * Library Version :ST_V3.5
- * Author		   :Christian
- * Web Site		   :www.seengreat.com
-***************************************************************************************/
+
 #include "spi.h"
 
 void SPI1_Init(void)
