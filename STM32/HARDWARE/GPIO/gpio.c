65767f3de27545f8bc7bb8c074709dee7ed0dbe6
seengreat <110515049+seengreat@users.noreply.github.com>
2022-12-01 19:43:13 +0800

    Update gpio.c

--- STM32/HARDWARE/GPIO/gpio.c
+++ STM32/HARDWARE/GPIO/gpio.c
@@ -1,19 +1,3 @@
-/***************************************************************************************
- * Project  :RC522-SPI-STM32
- * Describe :Read the UID of the S50 card and print the card number
- *			 Read block 8 data and change the first four bytes to 0x11, 0x22, 0x33, 0x44
- * Experimental Platform :STM32F103C8T6 + RC522 RFID HAT
- * Hardware Connection :STM32F103 -> RC522 RFID HAT
- *			5V	 ->	5V					PB12 -> CE0
- *			GND	 ->	GND					PB13 -> SCK
- *			PA8	 ->	P25					PB14 -> MISO
- *			PB4  ->	P29					PB15 -> MOSI
- *			PB5	 ->	P24
- *			
- * Library Version :ST_V3.5
- * Author		   :Christian
- * Web Site		   :www.seengreat.com
-***************************************************************************************/
 #include "gpio.h"
 #include "spi.h"
 
